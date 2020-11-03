7aceb55279b4e17a9d35a5132eaccb3f4ac12f45
andre ferrer <andrerferrer@hotmail.com>
2020-11-03 05:18:02 -0300

    Fix css reference

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const btn = document.querySelector("button");
 
 btn.addEventListener('click', (event) => {
   btn.innerText = (btn.innerText === "night mode" ? 'day mode' : 'night mode');
-  document.body.classList.toggle('dark');
+  document.body.classList.toggle('bg-dark');
 })
