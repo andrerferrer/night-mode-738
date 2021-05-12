6333eb47f3f9bfd370e03e63ae04380e8edae1f9
andre ferrer <andrerferrer@hotmail.com>
2021-05-12 03:33:51 -0300

    update it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-const btn = document.querySelector("button");
-
-btn.addEventListener('click', (event) => {
-  btn.innerText = (btn.innerText === "night mode" ? 'day mode' : 'night mode');
-  document.body.classList.toggle('bg-dark');
-})
+// Todo
